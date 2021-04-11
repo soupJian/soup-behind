a78d38953467c567140e27da4d5ea81ced2a5ec1
soupJian <soupjian@163.com>
2021-04-11 23:13:42 +0800

    feat：实现数据库连接，封装promise

---
+++ mysql/mysql.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql')
+
+const  options = {
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'root',
+  database: 'study'
+}
+// 创建与数据库的连接对象
+var connection = mysql.createConnection(options)
+
+// 建立连接
+connection.connect((err)=>{
+  // 如果建立失败
+  if(err){
+    console.log("数据库连接失败");
+  }else{
+    console.log("连接数据库成功");
+  }
+})
+
+const mysqlRequest = (sql) =>{
+  return new Promise((resolve,reject)=>{
+    connection.query(sql,(err,result)=>{
+      if(err){
+        reject(err)
+        console.log("查询异常");
+      }else{
+        resolve(result)
+      }
+    })
+  })
+}
+module.exports = mysqlRequest;
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const mysqlRequest = require('../mysql/mysql')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const sql = 'select * from user'
-  mysqlRequest(sql)
   res.send('respond with a resource');
 });
 
